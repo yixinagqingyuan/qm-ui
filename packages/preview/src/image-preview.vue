a8aae1819f03be58dd5a20403b91707b9d1089f7
好学习吧丶 <1245997863@@qq.com>
2021-07-19 23:47:37 +0800

    feat  :提交代码

--- packages/preview/src/image-preview.vue
+++ packages/preview/src/image-preview.vue
@@ -1,10 +1,11 @@
 <template>
-	<transition @after-leave="handleAfterLeave">
-		<div>aaaa</div>
-	</transition>
+  <transition @after-leave="handleAfterLeave">
+    <div>aaaa</div>
+    天天形象上
+  </transition>
 </template>
 <script>
-	export default {};
+export default {};
 </script>
 <style>
 </style>
